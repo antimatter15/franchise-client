3cc88b5d20ad432fc44f9af5fa21759f74cd4d7c
Kevin Kwok <antimatter15@gmail.com>
2017-07-12 15:04:37 -0700

    franchise-client

---
+++ server.js
@@ -0,0 +1,20 @@
+const WebSocket = require('ws');
+const port = parseInt('bat', 36); // 14645, as in batman (the movie franchise)
+const wss = new WebSocket.Server({ port: port });
+ 
+wss.on('connection', function connection(ws) {
+	console.log('opened connection')
+	ws.on('message', function incoming(message) {
+		console.log('received: %s', message);
+		setTimeout(function(){
+			ws.send(JSON.stringify({
+				test: 'hello'
+			}));
+		}, 100)
+	});
+	ws.send(JSON.stringify({
+		ready: true
+	}));
+});
+
+console.log("listening on port " + port)
