c197a837dc1551e18ed69593fe8fdff9eeb4b947
Kevin Kwok <antimatter15@gmail.com>
2017-07-12 15:05:55 -0700

    not using es6

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
-const WebSocket = require('ws');
-const port = parseInt('bat', 36); // 14645, as in batman (the movie franchise)
-const wss = new WebSocket.Server({ port: port });
+var WebSocket = require('ws');
+var port = parseInt('bat', 36); // 14645, as in batman (the movie franchise)
+var wss = new WebSocket.Server({ port: port });
  
 wss.on('connection', function connection(ws) {
 	console.log('opened connection')
