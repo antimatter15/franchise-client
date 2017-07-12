2c2ed3d6d853b551f37ebdd45eba78a7c6de16dd
Kevin Kwok <antimatter15@gmail.com>
2017-07-12 15:11:00 -0700

    adding hashbang

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
+#!/usr/bin/env node
+
 var WebSocket = require('ws');
 var port = parseInt('bat', 36); // 14645, as in batman (the movie franchise)
 var wss = new WebSocket.Server({ port: port });
- 
+
 wss.on('connection', function connection(ws) {
 	console.log('opened connection')
 	ws.on('message', function incoming(message) {
