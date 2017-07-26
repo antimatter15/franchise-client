b76460fa7598b4bb42ed44693d1c61116f8b5baa
guillermo webster <guillermo@cdbzb.com>
2017-07-26 16:58:11 -0700

    refactor to support direct usage in electron

--- server.js
+++ server.js
@@ -3,46 +3,24 @@
 const WebSocket = require('ws');
 const port = parseInt('bat', 36); // 14645, as in batman (the movie franchise)
 
-const { Client } = require('pg')
+const handle = require('./rpc.js')
+const creds = require('./credentials.js')
 
-const wss = new WebSocket.Server({ port: port });
-console.log("listening on port " + port)	
+const wss = new WebSocket.Server({ port });
+console.log("listening on port", port)
 
 wss.on('connection', ws => {
 	console.log('opened connection')
+
+	ws.send(JSON.stringify(creds))
 	
-	ws.on('message', async message => {
+	ws.on('message', message => {
 		console.log('received:', message);
 
-
 		message = JSON.parse(message)
+		const {id} = message
 
-		const {action, id} = message
-		const reply = msg => ws.send(JSON.stringify(Object.assign({id}, msg)))
-
-		if(action === 'open'){
-			const {credentials} = message
-
-			ws.client = new Client(credentials)
-
-			try {
-				await new Promise((resolve, reject) => ws.client.connect(err => err ? reject() : resolve()))
-			} catch(e) {
-				return reply({ready: false, error: e})
-			}
-
-			reply({ready: true})
-		} else if(action === 'exec'){
-			const {sql} = message
-
-			try {
-				const results = await ws.client.query(sql)
-				reply({results})
-			} catch (e) {
-				reply({error: e.stack.split('\n')[0]})
-			}
-
-		}
+		handle(message, res => ws.send(JSON.stringify(Object.assign({id}, res))))
 
 	})
 })
